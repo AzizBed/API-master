478e737409a3266b630474321ed4958c5290796c
AzizBed <abedouihech93@gmail.com>
2021-06-06 01:47:27 +0100

    first commit

---
+++ src/Component/UserCard/UserItem.js
@@ -0,0 +1,55 @@
+import { Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./UserItem.css"
+
+function UserItem({ info, el }) {
+    return (
+        <div>
+            <Card
+            className="usercard"
+                style={{
+                    width: "18rem",
+                    background: "gold",
+                    color: "grey",
+                    height: "450px",
+                    marginTop: "20px",
+                }}
+            >
+                <Card.Body>
+                    <Card.Title>{info.name}</Card.Title>
+                    <Card.Text>
+                        <b>Username :</b> {info.username}
+                    </Card.Text>
+                    <hr />
+
+                    <Card.Text>
+                        <b>Company :</b> {info.company.name}
+                    </Card.Text>
+                    <hr />
+                    <Card.Text>
+                        <b>Catch phrase :</b> {info.company.catchPhrase}
+                    </Card.Text>
+
+                    <hr />
+                    <Card.Text>
+                        <b>Phone :</b> {info.phone}
+                    </Card.Text>
+                    <Card.Text>
+                        <hr />
+                        <b>Adress : </b> {info.address.street}
+                    </Card.Text>
+                    <Card.Text>
+                        <b>Zip code :</b>
+                        {info.address.zipcode}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    {" "}
+                    <Card.Link href="#">{info.email}</Card.Link>
+                </Card.Footer>
+            </Card>
+        </div>
+    );
+}
+
+export default UserItem;
